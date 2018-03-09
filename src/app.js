12a46a76120ae275e4bcca09e1013e457feedada
Res Yeg <shaqk702@gmail.com>
2018-03-09 09:05:54 -0800

    logic done, debugging

---
+++ src/app.js
@@ -0,0 +1,111 @@
+// You're going to create a server using Node and Express. 
+// This server is going to have the endpoint /compare and will return the amount 
+// that bitcoin's value in USD has either risen or fallen between the current day and the previous. 
+// You will get this data by using the provided api. 
+// Make two separate requests, one for the previous value and one for the current.
+const express = require('express');
+const fetch = require('node-fetch');
+
+const app = express();
+const PORT = 3040;
+const STATUS_SUCCESS = 200;
+const STATUS_USER_ERROR = 422;
+const CUR_BPI_URI = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+const PREV_BPI_URI = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
+
+app.get('compare', (req, res) => {
+    fetch(PREV_BPI_URI)
+    .then(prevBpi => prevBpi.json())
+    .then(prevBpi => {
+        fetch(CUR_BPI_URI)
+        .then(curBpi => curBpi.json())
+        .then(curBpi => {
+            const diff = curBpi - prevBpi;
+            if (diff < 0) {
+                res.status(STATUS_SUCCESS);
+                res.send(`Bitcoin price fell by ${diff}`);
+            } else if (diff === 0) {
+                res.status(STATUS_SUCCESS);
+                res.send('Bitcoin price has not changed');
+            } else {
+                res.status(STATUS_SUCCESS);
+                res.send(`Bitcoin price rose by ${diff}`);
+            }
+        })
+    })
+    .catch(err => {
+        res.status(STATUS_USER_ERROR);
+        res.send( {err: err} );
+      });
+});
+
+// app.get('/place', (req, res) => {
+//   let search = req.query.search.split(' ');
+//   search = search.join('+');
+//   const searchUrl = URI_TEXT_SEARCH + search + '&key=' + KEY_GMAPS_PLACES;
+//   console.log("url", searchUrl);
+//   fetch(searchUrl, { method: 'GET'})
+//     .then(places => places.json())
+//     .then(places => {
+//       const placeId = places.results[0].place_id;
+//       const detailsUrl = URI_PLACE_DETAILS + placeId + '&key=' + KEY_GMAPS_PLACES;
+//       fetch(detailsUrl)
+//         .then(details => details.json())
+//         .then(details => {
+//           res.status(STATUS_SUCCESS);
+//           res.send(details.result);
+//         })
+//         .catch(err => {
+//           console.log(err);
+//           res.status(STATUS_USER_ERROR);
+//           res.send({ err: err} );
+//         });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(STATUS_USER_ERROR);
+//       res.send( {err: err} );
+//     });
+// });
+
+// app.get('/places', (req, res) => {
+//   const search = req.query.search;
+//   const searchUrl = URI_TEXT_SEARCH + search + '&key=' + KEY_GMAPS_PLACES;
+
+//   fetch(searchUrl)
+//     .then(places => places.json())
+//     .then(places => {
+//       placeIds = places.results.map(place => place.place_id);
+//       details = placeIds.map(id => {
+//         const detailsUrl = URI_PLACE_DETAILS + id + '&key=' + KEY_GMAPS_PLACES;
+//         return fetch(detailsUrl)
+//           .then(detailed => detailed.json())
+//           .then(detailed => detailed.result);
+//       });
+
+//       Promise.all(details)
+//         .then(details => {
+//           res.status(STATUS_SUCCESS);
+//           res.send( {places: details} )
+//         })
+//         .catch(err => {
+//           console.log(err)
+//           res.status(STATUS_USER_ERROR);
+//           res.send( {err: err});
+//         });
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.status(STATUS_USER_ERROR);
+//       res.send( {err: err} );
+//     });
+// });
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(`Error starting server: ${err}`);
+  } else {
+    console.log(`App listening on port ${PORT}`);
+  }
+});
+
