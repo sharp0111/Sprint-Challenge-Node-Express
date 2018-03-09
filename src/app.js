b6497da9559e205b7b952701d2389eb96add6caa
Res Yeg <shaqk702@gmail.com>
2018-03-09 09:27:20 -0800

    done

--- src/app.js
+++ src/app.js
@@ -13,23 +13,32 @@ const STATUS_USER_ERROR = 422;
 const CUR_BPI_URI = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 const PREV_BPI_URI = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
 
-app.get('compare', (req, res) => {
+app.get('/compare', (req, res) => {
     fetch(PREV_BPI_URI)
     .then(prevBpi => prevBpi.json())
     .then(prevBpi => {
+        const prevBpiObj = prevBpi.bpi;
+        let prevBpiValue = 0;
+        for (let key in prevBpiObj) {
+            if (prevBpiObj.hasOwnProperty(key)) {
+                preBpiValue = prevBpiObj[key];
+            }
+        }
         fetch(CUR_BPI_URI)
         .then(curBpi => curBpi.json())
         .then(curBpi => {
-            const diff = curBpi - prevBpi;
-            if (diff < 0) {
+            const curBpiValue = curBpi.bpi.USD.rate_float;
+            const diffNum = curBpiValue - preBpiValue;
+            const diffStr = diffNum.toFixed(4);
+            if (diffNum < 0) {
                 res.status(STATUS_SUCCESS);
-                res.send(`Bitcoin price fell by ${diff}`);
-            } else if (diff === 0) {
+                res.send(`Bitcoin price fell by ${diffStr}`);
+            } else if (diffNum === 0) {
                 res.status(STATUS_SUCCESS);
                 res.send('Bitcoin price has not changed');
             } else {
                 res.status(STATUS_SUCCESS);
-                res.send(`Bitcoin price rose by ${diff}`);
+                res.send(`Bitcoin price rose by ${diffStr}`);
             }
         })
     })
